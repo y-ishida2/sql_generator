c593144f74e0ba919cc5a0fe55f348f39e74e9df
y_ishida2 <y_ishida2@ga-tech.co.jp>
2019-03-11 11:33:44 +0900

    rename dir_name and method_name

--- sql_generator/practice/created_sql/test_table.sql
+++ sql_generator/practice/created_sql/test_table.sql
@@ -1,1 +1,1 @@
-INSERT INTO test_table VALUES ('hoge', 'piyo');
+INSERT INTO test_table ('column_name_1', 'column_name_2') VALUES ('hoge', 'piyo');
--- sql_generator/practice/sample_run.rb
+++ sql_generator/practice/sample_run.rb
@@ -2,15 +2,15 @@ require 'yaml'
 
 class SqlGenerator
   def initialize(file_name)
-    table_data = YAML.load_file("./table_info/#{file_name}")
+    table_data = YAML.load_file("./table_data/#{file_name}")
     @table_name = table_data['table_name']
   end
 
   def generate_sql
-    "INSERT INTO #{@table_name} VALUES ('hoge', 'piyo');"
+    "INSERT INTO #{@table_name} ('column_name_1', 'column_name_2') VALUES ('hoge', 'piyo');"
   end
 
-  def touch_file
+  def generate_file
     File.open("./created_sql/#{@table_name}.sql", "w") do |f|
       f.puts(self.generate_sql)
     end
@@ -18,5 +18,5 @@ class SqlGenerator
 end
 
 sql_generator = SqlGenerator.new(ARGV[0])
-sql_generator.touch_file
+sql_generator.generate_file
 
