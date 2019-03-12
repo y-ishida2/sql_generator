bec63ce19266bfee6f9bb86612b35673cf2cdd50
y_ishida2 <y_ishida2@ga-tech.co.jp>
2019-03-12 13:33:50 +0900

    add error_table.yml and argument_handling_wip

--- sql_generator/run.rb
+++ sql_generator/run.rb
@@ -2,6 +2,10 @@ require 'yaml'
 
 class SqlGenerator
   def initialize(file_name)
+    unless File.exist?("./table_data/#{file_name}")
+      puts 'ファイル名が正しくありません。'
+      exit!
+    end
     table_data = YAML.load_file("./table_data/#{file_name}")
     @table_name = table_data['table_name']
     @table_counts = table_data['counts']
@@ -15,7 +19,8 @@ class SqlGenerator
     puts "end======================================"
     @columns_values = table_data['columns'].values.to_s
   rescue
-    puts "ファイル名が正しくありません。"
+    puts "table_dataの形式が正しくありません。"
+    exit!
   end
 
   def generate_sql
@@ -32,6 +37,24 @@ class SqlGenerator
 end
 
 if __FILE__ == $0
+  unless ARGV.size == 1
+    puts 'usage: $ ruby run.rb <file_name>'
+    exit!
+  end
+ #  unless File.exist?("./table_data/#{ARGV[0]}")
+ #    puts 'ファイル名が正しくありません。'
+ #    exit!
+ #  end
   SqlGenerator.new(ARGV[0]).generate_file
+
+#   if ARGV.size != 1
+#     puts 'usage: $ ruby run.rb <file_name>'
+#     exit!
+#   elsif File.exist?("./table_data/#{ARGV[0]}")
+#     SqlGenerator.new(ARGV[0]).generate_file
+#   else
+#     puts 'ファイル名が正しくありません。'
+#     exit!
+#   end
 end
 
