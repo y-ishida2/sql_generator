9d8dfc6cdac157232e6d95e7a328b417f8064393
y_ishida2 <y_ishida2@ga-tech.co.jp>
2019-03-13 17:09:51 +0900

    add create_random_str_method

--- run.rb
+++ run.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'securerandom'
 
 class SqlGenerator
   def initialize(file_name)
@@ -10,30 +11,66 @@ class SqlGenerator
 
     begin
       @table_name = table_data['table_name']
-      @table_counts = table_data['counts']
+      @record_counts = table_data['counts']
       @columns_keys = table_data['columns'].keys.join(', ')
       @columns_values = table_data['columns'].values.to_s.gsub(/[\[\]]/, '').gsub(/"/, '\'')
+      @columns_values_test = table_data['columns'].values
     rescue
       raise 'table_dataの形式が正しくありません。'
     end
-
-    # puts "begin======================================"
-    # puts table_data['columns'].values
-    # puts table_data['columns'].values.to_s
-    # puts table_data['columns'].values.to_s.gsub(/[\[\]]/, '')
-    # puts table_data['columns'].values.to_s.gsub(/[\[\]]/, '').gsub(/"/, '\'')
-    # puts "end======================================"
-  end
-
-  def generate_sql
-    "INSERT INTO #{@table_name} (#{@columns_keys}) VALUES (#{@columns_values});\n" * @table_counts
   end
 
   def generate_file
     File.open("./created_sql/#{@table_name}.sql", "w") do |f|
-      f.puts(self.generate_sql)
+      f.puts(generate_sql)
     end
     puts "sql_fileが作られました！"
+    # random_str
+  end
+
+  private
+
+  def random_str
+    str = ''
+    @record_counts.times do
+      @columns_values_test[0] = SecureRandom.alphanumeric(10)
+      str << "INSERT INTO #{@table_name} (#{@columns_keys}) VALUES (#{@columns_values_test});\n".gsub(/[\[\]]/, '').gsub(/"/, '\'')
+    end
+    puts str
+  end
+
+  def generate_sql
+    # "INSERT INTO #{@table_name} (#{@columns_keys}) VALUES (#{@columns_values});\n" * @record_counts
+
+    ### random_str
+    str = ''
+    @record_counts.times do
+      @columns_values_test[0] = SecureRandom.alphanumeric(10)
+      str << "INSERT INTO #{@table_name} (#{@columns_keys}) VALUES (#{@columns_values_test});\n".gsub(/[\[\]]/, '').gsub(/"/, '\'')
+    end
+    str
+
+    ### times
+    # str = ''
+    # @record_counts.times do
+    #   str << "INSERT INTO #{@table_name} (#{@columns_keys}) VALUES (#{@columns_values});\n"
+    # end
+    # str
+
+    ### each
+    # str = ''
+    # (1..@record_counts).each do
+    #   str << "INSERT INTO #{@table_name} (#{@columns_keys}) VALUES (#{@columns_values});\n"
+    # end
+    # str
+
+    ### 初期値が欲しい時
+    # str = ''
+    # ini = 10
+    # (ini..@record_counts+ini-1).each do
+    #   str << "INSERT INTO #{@table_name} (#{@columns_keys}) VALUES (#{@columns_values});\n"
+    # end
+    # str
   end
 end
 
