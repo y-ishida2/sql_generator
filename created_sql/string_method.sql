54d08675bec875604fd6fb4940b0afbc41b1795f
y_ishida2 <y_ishida2@ga-tech.co.jp>
2019-03-18 16:39:00 +0900

    change case to method

---
+++ created_sql/string_method.sql
@@ -0,0 +1,10 @@
+INSERT INTO string_method (string, string_2, serial) VALUES ('Vowqrv', 'てりきとせすおみなき', 20);
+INSERT INTO string_method (string, string_2, serial) VALUES ('EzmYiu', 'ふよやすをなむかもく', 21);
+INSERT INTO string_method (string, string_2, serial) VALUES ('1aK2Rq', 'けれぬちめてくめわる', 22);
+INSERT INTO string_method (string, string_2, serial) VALUES ('vjyX7x', 'きえるこおねゆないゆ', 23);
+INSERT INTO string_method (string, string_2, serial) VALUES ('nl093J', 'えすはてをむろねさう', 24);
+INSERT INTO string_method (string, string_2, serial) VALUES ('EPDZpd', 'えねんゆかゆわやおの', 25);
+INSERT INTO string_method (string, string_2, serial) VALUES ('kkrrFQ', 'うええもせめくきあみ', 26);
+INSERT INTO string_method (string, string_2, serial) VALUES ('fCSF4r', 'れそとはのりのめほえ', 27);
+INSERT INTO string_method (string, string_2, serial) VALUES ('vUxD2E', 'ひむのはよさけるゆつ', 28);
+INSERT INTO string_method (string, string_2, serial) VALUES ('GmhVVj', 'きめらよくおをてのえ', 29);
--- run.rb
+++ run.rb
@@ -32,36 +32,49 @@ class SqlGenerator
     @record_counts.times do |i|
       columns_values = []
       @columns_values.each do |value|
-        if value.kind_of?(String)
-          case value
-          when /random/
-            size = value.match(/\d+/)[0].to_i
-            # value = SecureRandom.alphanumeric(size)
-            value = random_char(size)
-          when /serial_num/
-            ini = value.match(/\d+/)[0].to_i
-            value = ini + i
-          when /hiragana/
-            size = value.match(/\d+/)[0].to_i
-            value = random_hiragana(size)
-          end
-        end
-        columns_values << value
+        columns_values << send(value[0], value[1], i)
       end
       str << "INSERT INTO #{@table_name} (#{@columns_keys}) VALUES (#{columns_values});\n".gsub(/[\[\]]/, '').gsub(/"/, '\'')
     end
     str
+
+    # str = ''
+    # @record_counts.times do |i|
+    #   columns_values = []
+    #   @columns_values.each do |value|
+    #     if value.kind_of?(String)
+    #       case value
+    #       when /random/
+    #         size = value.match(/\d+/)[0].to_i
+    #         # value = SecureRandom.alphanumeric(size)
+    #         value = random_char(size)
+    #       when /serial_num/
+    #         ini = value.match(/\d+/)[0].to_i
+    #         value = ini + i
+    #       when /hiragana/
+    #         size = value.match(/\d+/)[0].to_i
+    #         value = random_hiragana(size)
+    #       end
+    #     end
+    #     columns_values << value
+    #   end
+    #   str << "INSERT INTO #{@table_name} (#{@columns_keys}) VALUES (#{columns_values});\n".gsub(/[\[\]]/, '').gsub(/"/, '\'')
+    # end
+    # str
   end
 
-# 現状arrayのsizeを超えるとダメ
-  def random_char(size)
+  def random_char(size, i)
     character = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
     (0...size).map { character[rand(character.size)] }.join
   end
 
-  def random_hiragana(size)
+  def random_hiragana(size, i)
     hiragana = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふヘほまみむめもやゆよらりるれろわをん'.split('')
     (0...size).map { hiragana[rand(hiragana.size)] }.join
+  end
+
+  def serial_num(ini, i)
+    ini + i
   end
 end
 
